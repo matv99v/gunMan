d6cddb4bf4929d7c00b4c0b2aa05be431201fc6e
Vladimir Matveev <matv99v@gmail.com>
2015-12-14 02:16:29 +0200

    decreased sounds amount

--- dist/bundle.js
+++ dist/bundle.js
@@ -531,19 +531,19 @@
 /***/ function(module, exports) {
 
 	/* WEBPACK VAR INJECTION */(function(global) {var sounds = {
-		bonusPoint : new Audio('./src/sounds/bonus-point.mp3'),
-		doorOpening : new Audio('./src/sounds/door-opening.mp3'),
+		// bonusPoint : new Audio('./src/sounds/bonus-point.mp3'),
+		// doorOpening : new Audio('./src/sounds/door-opening.mp3'),
 		fire : new Audio('./src/sounds/fire.mp3'),
 		foul : new Audio('./src/sounds/foul.mp3'),
-		gameOver : new Audio('./src/sounds/game-over.mp3'),
-		guyFalling : new Audio('./src/sounds/guy-falling.mp3'),
+		// gameOver : new Audio('./src/sounds/game-over.mp3'),
+		// guyFalling : new Audio('./src/sounds/guy-falling.mp3'),
 		guyHitFloor : new Audio('./src/sounds/guy-hit-floor.mp3'),
 		oneOutlawIntro : new Audio('./src/sounds/one-outlaw-intro.mp3'),
-		prepareToPlay : new Audio('./src/sounds/prepare-to-play.mp3'),
+		// prepareToPlay : new Audio('./src/sounds/prepare-to-play.mp3'),
 		prepareToShoot : new Audio('./src/sounds/prepare-to-shoot.mp3'),
 		shoot : new Audio('./src/sounds/shoot.mp3'),
-		title : new Audio('./src/sounds/title.mp3'),
-		twoOutlawsIntro : new Audio('./src/sounds/two-outlaws-intro.mp3'),
+		// title : new Audio('./src/sounds/title.mp3'),
+		// twoOutlawsIntro : new Audio('./src/sounds/two-outlaws-intro.mp3'),
 		youLost : new Audio('./src/sounds/you-lost.mp3'),
 		youWon : new Audio('./src/sounds/you-won.mp3'),
 	
